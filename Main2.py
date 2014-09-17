4b79b58013862f406fed923f1e13fe2e9b7a6131
Cihat YILDIZ <cihat.yildiz@gmail.com>
2014-09-17 13:58:17 +0300

    Revert "files_added"
    This reverts commit b97c2516af560420f235496ae4d8cc62658d0f35.

--- Main2.py
+++
@@ -1,7 +0,0 @@
-from FTP_Fuzzer import *
-
-fuzz = NetworkFuzzer("C:\Documents and Settings\Administrator\Desktop\FTPServer.exe","192.168.96.128",21,"FTP",1,0.5)
-fuzz.start()
-
-while True:
-    pass
