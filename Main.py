4d2c7a665339c8eb40fbf9e955ccffe38c35bfa4
Cihat YILDIZ <cihat.yildiz@gmail.com>
2014-09-17 14:04:08 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 from FTP_Fuzzer import *
 
-fuzz = NetworkFuzzer("C:\Documents and Settings\Administrator\Desktop\FTPServer.exe","192.168.96.128",21,"FTP",1,0.5)
+fuzz = NetworkFuzzer("FTPServer.exe","127.0.0.1",21,"FTP",1,0.5)
 fuzz.start()
 
 while True:
